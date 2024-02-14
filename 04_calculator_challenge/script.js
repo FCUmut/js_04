fd7af2047930bb85a9224e0598b8ab510195cbae
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-02-14 23:21:50 +0300

    commit
    commit

---
+++ 04_calculator_challenge/script.js
@@ -0,0 +1,55 @@
+// Solution - 1
+function calculator(x, y, operator) {
+  if (
+    !(
+      operator === "+" ||
+      operator === "-" ||
+      operator === "*" ||
+      operator === "/"
+    )
+  ) {
+    console.log("Error: Please enter '+, -, * or /' operators");
+  } else {
+    switch (operator) {
+      case "+":
+        console.log(x + y);
+        break;
+      case "/":
+        console.log(x / y);
+        break;
+      case "-":
+        console.log(x - y);
+        break;
+      case "*":
+        console.log(x * y);
+        break;
+      default:
+        console.log("how you reached here!?");
+    }
+  }
+}
+
+// // Solution - 2
+// function calculator(num1, num2, operator) {
+//   let result;
+//   switch (operator) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "/":
+//       result = num1 / num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     default:
+//       result = "Invalid Operator";
+//   }
+//   console.log(result);
+//   return result;
+// }
+
+calculator(5, 2, "&");
